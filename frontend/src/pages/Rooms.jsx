427b47d3d7d1c3b0cea16013741229e046054aab
Harshil Shah <harshilshah980@gmail.com>
2026-01-19 10:43:33 +0000

    added filter layout

--- frontend/src/pages/Rooms.jsx
+++ frontend/src/pages/Rooms.jsx
@@ -92,7 +92,7 @@ const Rooms = () => {
                 <div className = {`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
                     <p className = "text-base font-medium text-gray-800">FILTERS</p>
                     <div className = "text-xs cursor-pointer">
-                        <span onClick={() => setOpenFilters(!openFilters)} className = "lg:hidden">{openFilters ? "HIDE" : "SHOW"}HIDE</span>
+                        <span onClick={() => setOpenFilters(!openFilters)} className = "lg:hidden">{openFilters ? "HIDE" : "SHOW"}</span>
                         <span className = "hidden lg:block">CLEAR</span>
                     </div>
                 </div>
@@ -100,6 +100,21 @@ const Rooms = () => {
                 <div className = {`${openFilters ? "h-auto" : "h-0 lg:h-auto"} overflow-hidden transition-all duration-700`}>
                     <div className = "px-5 pt-5">
                         <p className = "font-medium text-gray-800 pb-2">Popular filters</p>
+                        {roomTypes.map((room, index) => ( 
+                            <CheckBox key = {index} label = {room}/>
+                        ))}
+                    </div>
+                    <div className = "px-5 pt-5">
+                        <p className = "font-medium text-gray-800 pb-2">Price Range</p>
+                        {priceRanges.map((range, index) => (
+                            <CheckBox key = {index} label = {`$ ${range}`}/>
+                        ))}
+                    </div>
+                    <div className = "px-5 pt-5 pb-7">
+                        <p className = "font-medium text-gray-800 pb-2">Sort By</p>
+                        {sortOptions.map((option, index) => (
+                            <RadioButton key = {index} label = {option}/>
+                        ))}
                     </div>
                 </div>
             </div>
