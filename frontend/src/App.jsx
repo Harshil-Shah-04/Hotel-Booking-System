330375564e72170cba920403e2e69668ab089f34
Harshil Shah <harshilshah980@gmail.com>
2026-01-21 19:39:10 +0000

    Create structure for owner view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,35 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import RoomDetails from './pages/RoomDetails'
 import Bookings from './pages/Bookings'
+import HotelRegistration from './components/HotelRegistration'
+import Layout from './pages/hotel-owner/Layout'
 
 const App = () => {
     
+    const isOwnerPath = useLocation().pathname.includes("/owner");
+    
     return (
         <div className="min-h-screen flex flex-col">
-            <Navbar />
+            {!isOwnerPath && <Navbar/>}
+            {false && <HotelRegistration/>}
             
             <main className="flex-grow">
                 <div>
                     <Routes>
-                        <Route path="/" element={<Home/>} />
-                        <Route path="/rooms" element={<Rooms/>} />
-                        <Route path="/rooms/:id" element={<RoomDetails/>} />
-                        <Route path="/my-bookings" element={<Bookings/>} />
+                        <Route path='/' element={<Home/>} />
+                        <Route path='/rooms' element={<Rooms/>} />
+                        <Route path='/rooms/:id' element={<RoomDetails/>} />
+                        <Route path='/my-bookings' element={<Bookings/>} />
+                        <Route path='/owner' element={<Layout/>}></Route>
                     </Routes>
                 </div>
             </main>
             
-            <Footer />
+            <Footer/>
         </div>
     )
 }
