d24c7149a389393ef00e45473d50c6bc20bfcb52
Harshil Shah <harshilshah980@gmail.com>
2026-01-24 12:02:47 +0000

    Connect Clerk Webhook

---
+++ backend/controllers/clerkWebhooks.js
@@ -0,0 +1,59 @@
+import { Webhook } from "svix"
+import User from "../models/User.js"
+
+const clerkWebhooks = async (req, res) => {
+    try {
+        // Create a Svix instance with Clerk webhook secret
+        const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        
+        // Requesting Headers
+        const headers = {
+            "svix-id": req.header["svix-id"],
+            "svix-timestamp": req.header["svix-timestamp"],
+            "svix-signature": req.header["svix-signature"],
+        }
+        
+        // Verifying Headers
+        await webhook.verify(JSON.stringify(req.body), headers)
+        
+        // Getting Data from request body
+        const {data, type} = req.body
+        
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url
+        }
+        
+        // Switch cases for different Events
+        switch (type) {
+            
+            case "user.created": {
+                await User.create(userData)
+                break;
+            }
+            
+            case "user.updated": {
+                await User.findByIdAndUpdate(data.id, userData)
+                break;
+            }
+            
+            case "user.deleted": {
+                await User.findByIdAndDelete(data.id)
+                break;
+            }
+            
+            default:
+                break;
+        }
+        
+        res.json({success: true, message: "Webhook Recieved"})
+        
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export default clerkWebhooks
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,48 @@ import cors from "cors"
 import "dotenv/config"
 import connectDB from "./configs/db.js"
 import { clerkMiddleware } from "@clerk/express"
+import clerkWebhooks from "./controllers/clerkWebhooks.js"
 
 connectDB()
 
 const app = express()
 
 app.use(express.json())
-app.use(cors()) //Enable Cross-Origin Resource Sharing
+//Enable Cross-Origin Resource Sharing
+app.use(cors())
 app.use(clerkMiddleware())
+// API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks)
 
 app.get('/', (req, res) => res.send("API is working"))
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+// # Logs
+// logs
+// *.log
+// npm-debug.log*
+// yarn-debug.log*
+// yarn-error.log*
+// pnpm-debug.log*
+// lerna-debug.log*
+
+// node_modules
+// dist
+// dist-ssr
+// .env
+// *.local
+
+// # Editor directories and files
+// .vscode/*
+// !.vscode/extensions.json
+// .idea
+// .DS_Store
+// *.suo
+// *.ntvs*
+// *.njsproj
+// *.sln
+// *.sw?
