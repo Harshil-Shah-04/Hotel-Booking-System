c8b6c42b67f30afa944b1b0f4aee228fa791ff4e
Harshil Shah <harshilshah980@gmail.com>
2026-01-27 16:26:21 +0000

    Add more backend stuff

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,74 @@
+import Booking from "../models/Booking.js"
+import Room from "../models/Room.js"
+
+// Function to check availability of room
+const checkAvailability = async ({checkInDate, checkOutDate, room}) => {
+    try {
+        const bookings = await Booking.find({
+            room,
+            checkInDate: {$lts: checkOutDate},
+            checkOutDate: {$gte: checkInDate}
+        })
+        const isAvailable = bookings.length === 0
+        return isAvailable
+        
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// API to check availability of room
+export const checkAvailabilityAPI = async (req, res) => {
+    try {
+        const {room, checkInDate, checkOutDate} = req.body
+        const isAvailable = await checkAvailability({checkInDate, checkOutDate, room})
+        res.json({success: true, isAvailable})
+        
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+// API to create a new booking
+export const createBooking = async (req, res) => {
+    try {
+        // Before Booking check availability
+        const {room, checkInDate, checkOutDate, guests} = req.body
+        const user = req.user._id
+        
+        const isAvailable = await checkAvailability({checkInDate, checkOutDate, room})
+        
+        if (!isAvailable) {
+            return res.json({success: false, message: "Room is not available"})
+        }
+        
+        // Get total price for room
+        const roomData = await Room.findById(room).populate("hotel")
+        let totalPrice = roomData.pricePerNight
+        
+        // Calculate total price based on nights
+        const checkIn = new Date(checkInDate)
+        const checkOut = new Date(checkOutDate)
+        const timeDiff = checkOut.getTime() - checkIn.getTime()
+        const nights = Math.ceil(timeDiff / (1000 * 3600 * 24))
+        
+        totalPrice *= nights
+        const booking = await Booking.create({
+            user,
+            room,
+            hotel: roomData.hotel._id,
+            guests: +guests,
+            checkInDate,
+            checkOutDate,
+            totalPrice
+        })
+        
+        res.json({success: true, message: "Booking created successfully"})
+        
+    } catch (error) {
+        console.error(error);
+        res.json({success: false, message: "Failed to create Booking"})
+    }
+}
+
+// API to get all bookings for a user
--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,29 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./configs/db.js"
+import connectCloudinary from "./configs/cloudinary.js"
 import { clerkMiddleware } from "@clerk/express"
 import clerkWebhooks from "./controllers/clerkWebhooks.js"
 import userRouter from "./routes/userRoutes.js"
 import hotelRouter from "./routes/hotelRoutes.js"
+import roomRouter from "./routes/roomRoutes.js"
 
 connectDB()
+connectCloudinary()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use(clerkMiddleware())
 app.use("/api/clerk", clerkWebhooks)
 
 app.get('/', (req, res) => res.send("API is working"))
+
 app.use('/api/user', userRouter)
 app.use('/api/hotels', hotelRouter)
+app.use('/api/rooms', roomRouter)
 
 const PORT = process.env.PORT || 3000
 
